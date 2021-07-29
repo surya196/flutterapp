203767fd5e89a6624ae016735054c04ae8e7e131
surya <surya.m19698@gmail.com>
2021-07-29 15:01:08 +0530

    simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+backgroundColor: Colors.blueGrey[800],
+            title: Text('Dice '),
+          ),
+          body: DiceScreen(),
+        ),
+      ),
+    );
+  }
+}
+
+class DiceScreen extends StatefulWidget {
+  const DiceScreen({Key? key}) : super(key: key);
+
+  @override
+  _DiceScreenState createState() => _DiceScreenState();
+}
+
+class _DiceScreenState extends State<DiceScreen> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changeDice() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/dice$leftDice.png'),
+              )),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/dice$rightDice.png'),
+              )),
+            ],
+          ),
+          SizedBox(height: 20,),
+          RaisedButton(
+              child: Text('Roll'),
+              onPressed: () {
+            changeDice();
+          }
+          )
+        ],
+      ),
+    );
   }
 }
